a7d1db77a82907bda53b74397e886f41bf31ba7f
surya kuruvadi <107850687+SuryaCodez@users.noreply.github.com>
2023-10-11 22:52:56 +0530

    Create add.r
    Simple program to add two integers using R programming and returning the type of the final result variable.

---
+++ add.r
@@ -0,0 +1,6 @@
+a<-12L
+b<-5L #L is used at end to show it is an integer type variable
+c<-a+b
+cat("the result of the above operation is",c) #cat is used instead of using both print and paste in same line 
+print("\n")
+print(typeof(c))
