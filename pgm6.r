b00223fe6ead28fc902bc53fb68fc82ac066aafa
surya kuruvadi <107850687+SuryaCodez@users.noreply.github.com>
2024-01-30 14:02:37 +0530

    Create pgm6.r

---
+++ pgm6.r
@@ -0,0 +1,8 @@
+library(forecast)
+sales_data<-data.frame(
+  Month=seq(as.Date("2023-01-01"),as.Date("2023-06-01"),by="months"),
+  Sales=c(1200,1300,1450,1700,1800,2200)
+)
+sales_ts<-ts(sales_data$Sales,frequency = 12)
+arima_model<-auto.arima(sales_ts)
+print(forecast(arima_model,h=3))
